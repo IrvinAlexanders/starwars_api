4dea6fdbf08efbc87ee9fed7995cd93630706027
IrvinAlexanders <igarcia@hispasec.com>
2025-06-17 19:45:53 -0500

    feat: register models on Django Admin

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,10 @@
+from django.contrib import admin
+
 from .character import * # NoQA: F401, F403
 from .film import * # NoQA: F401, F403
 from .planet import * # NoQA: F401, F403
+
+
+admin.site.register(Character)
+admin.site.register(Film)
+admin.site.register(Planet)
